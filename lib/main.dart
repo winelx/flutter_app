cd90e0609a5a5199d0f1784c190f7d9a10a2c69f
winelx <1094290855@qq.com>
2019-03-14 14:39:02 +0800

    删除界面内容，仅保留appbar

--- lib/main.dart
+++ lib/main.dart
@@ -119,58 +119,6 @@ class Login extends State<myapp> {
             )
           ],
         ),
-        body: new Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            new Padding(
-              padding: new EdgeInsets.fromLTRB(
-                  leftRightPadding, 50.0, leftRightPadding, 10.0),
-              child: new Image.network(LOGO),
-            ),
-            new Padding(
-              padding: new EdgeInsets.fromLTRB(
-                  leftRightPadding, 50.0, leftRightPadding, topBottomPadding),
-              child: new TextField(
-                style: hintTips,
-                controller: _userNameController,
-                decoration: new InputDecoration(hintText: "请输入用户名"),
-                obscureText: true,
-              ),
-            ),
-            new Padding(
-              padding: new EdgeInsets.fromLTRB(
-                  leftRightPadding, 30.0, leftRightPadding, topBottomPadding),
-              child: new TextField(
-                style: hintTips,
-                controller: _userPassController,
-                decoration: new InputDecoration(hintText: "请输入用户密码"),
-                obscureText: true,
-              ),
-            ),
-            new Container(
-              width: 360.0,
-              margin: new EdgeInsets.fromLTRB(10.0, 40.0, 10.0, 0.0),
-              padding: new EdgeInsets.fromLTRB(leftRightPadding,
-                  topBottomPadding, leftRightPadding, topBottomPadding),
-              child: new Card(
-                color: Colors.green,
-                elevation: 6.0,
-                child: new FlatButton(
-                    onPressed: () {
-                      print("the pass is" + _userNameController.text);
-                    },
-                    child: new Padding(
-                      padding: new EdgeInsets.all(10.0),
-                      child: new Text(
-                        '马上登录',
-                        style:
-                            new TextStyle(color: Colors.white, fontSize: 16.0),
-                      ),
-                    )),
-              ),
-            )
-          ],
-        ));
+       );
   }
 }
